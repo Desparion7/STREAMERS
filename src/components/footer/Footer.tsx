eea5dea15d17a943d0e34cf68fbbc980512e38b9
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-06-28 14:42:22 +0200

    add footer and readme

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,11 @@
+import styles from './Footer.module.scss';
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className={styles.footer}>
+      <p className="gradient__text">Author Mateusz Woś</p>
+    </div>
+  );
 };
 
 export default Footer;
